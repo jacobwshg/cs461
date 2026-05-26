0be99f0fce30b6386715adf7b130a3ccfe6b6a6d
jacobwshg <wsg438@gmail.com>
2026-05-25 19:48:11 -0500

    [hw2-qa] added validate_aster script

---
+++ hw2-qa/validate_aster.bash
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+python3 Aster.py \
+	-loadname saved/model_best.pt \
+	-d_model 1024 \
+	-d_ff 4096 \
+	-n_layers 16 \
+	-heads 16 \
+	-seqlen 1024 \
+	-eval_batchsize 8 \
+	-valid_file saved/mixture_valid.txt \
+	-tokenizer saved/tokenizer 
+
